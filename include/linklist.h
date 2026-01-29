b54c43415e6da72a04125d10d492355ee0e4ab81
liyanbin2006 <3586916429@qq.com>
2026-01-29 12:35:46 +0800

    初始化项目结构并完成第一次运行成功

---
+++ include/linklist.h
@@ -0,0 +1,4 @@
+typedef struct Node{
+    int data;//存储数据
+    struct Node* next;//记录下一个节点的地址
+}Node;
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/linklist.h"
+
+int main() {
+    printf("我的数据结构项目 CoreDS 启动成功！\n");
+    return 0;
+}
